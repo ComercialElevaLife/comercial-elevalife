8d19edb49588ddc80419daa9a77c3b06cd7eca1a
ComercialElevaLife <leonardo@elevalife.com.br>
2026-05-26 13:41:37 -0300

    Delete PROJECT KPI/config.js

--- PROJECT KPI/config.js
+++
@@ -1,5 +0,0 @@
-window.KPI_CONFIG = window.KPI_CONFIG || {
-  supabaseUrl: "https://dkknxqdvdrgdxkhhfzxk.supabase.co/rest/v1/",
-  supabaseAnonKey: "sb_publishable_pMu4JZN7taAtqmbpirjucg_FtfOpqzB",
-  workspaceId: "comercial-elevalife",
-};
