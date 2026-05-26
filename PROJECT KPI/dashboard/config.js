e433db45b1e44baa6000821ab5ad7b2b6f008b07
ComercialElevaLife <leonardo@elevalife.com.br>
2026-05-26 13:31:16 -0300

    Create config.js

---
+++ PROJECT KPI/dashboard/config.js
@@ -0,0 +1,5 @@
+window.KPI_CONFIG = window.KPI_CONFIG || {
+  supabaseUrl: "https://dkknxqdvdrgdxkhhfzxk.supabase.co/rest/v1/",
+  supabaseAnonKey: "sb_publishable_pMu4JZN7taAtqmbpirjucg_FtfOpqzB",
+  workspaceId: "comercial-elevalife",
+};
